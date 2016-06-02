8c3d924c09ada5b6acc7f8b1dcac657ab5ec82e1
igor.rrsantos <igor.rrsantos@CAS419550W0707.senacsp.edu.br>
2016-06-02 20:09:50 -0300

    teste1

---
+++ src/Repeticao.java
@@ -0,0 +1,49 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author igor.rrsantos
+ */
+public class Repeticao {
+
+    public static void main(String[] args) {
+
+        
+        int i = 0;
+
+        while (i <= 50) {
+            
+
+            System.out.println("Esse Aluno é " + i);
+           i++; 
+        }
+
+    
+     /** int i = 1;
+        *  do{ 
+         *     
+          *    System.out.println(" Aluno  " + i);
+           *   i++;
+            *  
+          *}while (i <= 50); {
+            
+        *System.out.println("Acertou");
+            
+        }*/ 
+
+    }
+    
+    
+    
+    }
+
+    
+    
+    
+
